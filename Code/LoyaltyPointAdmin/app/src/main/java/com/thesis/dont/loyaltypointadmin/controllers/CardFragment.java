7ce9e94d1959a5f4d7e76556810fbab123299112
tinntt <ntttin132@gmail.com>
2015-03-12 01:09:55 +0700

    Finish tab and swipe in toolbar

---
+++ Code/LoyaltyPointAdmin/app/src/main/java/com/thesis/dont/loyaltypointadmin/controllers/CardFragment.java
@@ -0,0 +1,49 @@
+package com.thesis.dont.loyaltypointadmin.controllers;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.thesis.dont.loyaltypointadmin.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class CardFragment extends Fragment {
+    private static final String ARG_POSITION = "position";
+
+    @InjectView(R.id.textView)
+    TextView textView;
+
+    private int position;
+
+    public static CardFragment newInstance(int position) {
+        CardFragment f = new CardFragment();
+        Bundle b = new Bundle();
+        b.putInt(ARG_POSITION, position);
+        f.setArguments(b);
+        return f;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        position = getArguments().getInt(ARG_POSITION);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_card,container,false);
+        ButterKnife.inject(this, rootView);
+        ViewCompat.setElevation(rootView, 50);
+        textView.setText("CARD " + position);
+        return rootView;
+    }
+
+}
