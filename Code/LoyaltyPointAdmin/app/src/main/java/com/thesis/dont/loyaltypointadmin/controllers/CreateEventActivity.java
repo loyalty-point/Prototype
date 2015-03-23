90475d1cd35269d4a81d07086f4b23a49354659d
tinntt <ntttin132@gmail.com>
2015-03-23 22:49:05 +0700

    added create event

--- Code/LoyaltyPointAdmin/app/src/main/java/com/thesis/dont/loyaltypointadmin/controllers/CreateEventActivity.java
+++ Code/LoyaltyPointAdmin/app/src/main/java/com/thesis/dont/loyaltypointadmin/controllers/CreateEventActivity.java
@@ -1,40 +1,109 @@
 package com.thesis.dont.loyaltypointadmin.controllers;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.Spinner;
 
+import com.gc.materialdesign.views.ButtonFlat;
 import com.thesis.dont.loyaltypointadmin.R;
 
-public class CreateEventActivity extends ActionBarActivity {
+import java.util.Calendar;
+
+public class CreateEventActivity extends FragmentActivity implements DatePickerDialog.OnDateSetListener {
+    ButtonFlat dateStartButton, dateEndButton;
+    Spinner category;
+    boolean isStartDatePicker = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+        Calendar c = Calendar.getInstance();
+        category = (Spinner) findViewById(R.id.eventcategory);
+        /** create value to list and add event change frangment**/
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.eventCategory, R.layout.spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        category.setAdapter(adapter);
+        category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // Create new fragment and transaction
+                if(position == 1) {
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+                    fragmentTransaction.replace(R.id.fragment_container, new CreateEvent2Fragment());
+                    fragmentTransaction.addToBackStack(null);
+
+                    fragmentTransaction.commit();
+                }else {
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+                    fragmentTransaction.replace(R.id.fragment_container, new CreateEvent1Fragment());
+                    fragmentTransaction.addToBackStack(null);
+
+                    fragmentTransaction.commit();
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        /****/
+        dateStartButton = (ButtonFlat) findViewById(R.id.startDateButton);
+        dateEndButton = (ButtonFlat) findViewById(R.id.endDateButton);
+
+        dateStartButton.setText("start date\n" + c.get(Calendar.DAY_OF_MONTH) + "/" + (c.get(Calendar.MONTH) + 1) + "/" + c.get(Calendar.YEAR));
+        dateEndButton.setText("end date\n" + c.get(Calendar.DAY_OF_MONTH) + "/" + (c.get(Calendar.MONTH) + 1) + "/" + c.get(Calendar.YEAR));
+
+        dateStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getSupportFragmentManager(), "datePicker");
+                isStartDatePicker = true;
+            }
+        });
+
+        dateEndButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getSupportFragmentManager(), "datePicker");
+                isStartDatePicker = false;
+            }
+        });
+
+        if (findViewById(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+            CreateEvent1Fragment firstFragment = new CreateEvent1Fragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, firstFragment).commit();
+        }
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_create_event, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        if (isStartDatePicker) {
+            dateStartButton.setText("start date\n" + dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+        } else {
+            dateEndButton.setText("end date\n" + dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
         }
 
-        return super.onOptionsItemSelected(item);
     }
+
+
+//    @Override
+//    public void onFragmentInteraction(Uri uri) {
+//        Log.e("test","test");
+//    }
 }
