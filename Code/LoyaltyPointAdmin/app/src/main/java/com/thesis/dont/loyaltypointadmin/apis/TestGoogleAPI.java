cb83afd2ff04e05b86e35116358393899c289797
tinntt <ntttin132@gmail.com>
2015-07-15 20:05:08 +0700

    update project

--- Code/LoyaltyPointAdmin/app/src/main/java/com/thesis/dont/loyaltypointadmin/apis/TestGoogleAPI.java
+++
@@ -1,7 +0,0 @@
-package com.thesis.dont.loyaltypointadmin.apis;
-
-/**
- * Created by dont on 2/2/2015.
- */
-public class TestGoogleAPI {
-}
--- Code/LoyaltyPointAdmin/app/src/main/java/com/thesis/dont/loyaltypointadmin/views/MyButton.java
+++
@@ -1,9 +0,0 @@
-package com.thesis.dont.loyaltypointadmin.views;
-
-/**
- * Created by dont on 2/2/2015.
- */
-public class MyButton {
-	String text;
-	String testCommit;
-}
